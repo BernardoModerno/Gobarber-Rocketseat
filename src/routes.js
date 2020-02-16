a1c83f97edfc27e6fa171b9fdc7442f90f110fd4
Bernardo Moderno <bernardo.moderno@gmail.com>
2020-02-16 00:22:57 -0300

    Listando agenda do prestador.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -17,6 +18,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
