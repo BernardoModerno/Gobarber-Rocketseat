ffd241101ddb41ab40771d30165ae8647eb23f37
Bernardo Moderno <bernardo.moderno@gmail.com>
2020-02-17 21:12:47 -0300

    Configurando Nodemailder.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "1dfd0a4d60dcc4",
+    pass: "7a5c2964bbd4a2"
+  },
+  default: {
+    from: 'Equipe Gobarber <noreplay@gobarber.com>',
+  },
+};
