8a67f37a7de5aa15b8bd8cc6b17fc59a44cca01b
Bernardo Moderno <bernardo.moderno@gmail.com>
2020-02-21 21:36:15 -0300

    Configurando templates de email.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: "smtp.mailtrap.io",
+  host: 'smtp.mailtrap.io',
   port: 2525,
   secure: false,
   auth: {
-    user: "1dfd0a4d60dcc4",
-    pass: "7a5c2964bbd4a2"
+    user: '1dfd0a4d60dcc4',
+    pass: '7a5c2964bbd4a2',
   },
   default: {
     from: 'Equipe Gobarber <noreplay@gobarber.com>',
