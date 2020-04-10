045d9940e45687503c7e4bef0f4926b68b0ced3f
Bernardo Moderno <bernardo.moderno@gmail.com>
2020-04-09 21:32:18 -0300

    Ajustes na API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
-import routes from './routes'; // importando as rotas de outro arquivo
+import routes from './routes';
 import sentryConfig from './config/sentry';
 import './database';
 
@@ -19,6 +19,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,8 +1,4 @@
-import { startOfDay, endOfDay, parseISO } from 'date-fns';
-import { Op } from 'sequelize';
-import User from '../models/User';
 import Appointment from '../models/Appointment';
-
 class ScheduleController {
   async index(req, res) {
     const checkUserProvider = await User.findOne({
@@ -12,19 +8,26 @@ class ScheduleController {
       return res.status(401).json({ error: 'User is not a provider' });
     }
     const { date } = req.query;
-    const parseDate = parseISO(date);
+    const parsedDate = parseISO(date);
     const appointments = await Appointment.findAll({
       where: {
         provider_id: req.userId,
         canceled_at: null,
         date: {
-          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
+
     return res.json(appointments);
   }
 }
-
 export default new ScheduleController();
